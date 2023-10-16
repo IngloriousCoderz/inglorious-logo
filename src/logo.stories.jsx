70b8937bf3a19eafe34ffcf5dd90d03f38220a3a
Matteo Antony Mistretta <m.mistretta@reply.it>
2023-10-16 22:29:39 +0200

    Allow better transparency on export

--- src/logo.stories.jsx
+++ src/logo.stories.jsx
@@ -18,5 +18,21 @@ export const Interactive = {
 };
 
 export const IngloriousEngine = {
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          background: "black",
+          width: 320,
+          height: 240,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
   args: { size: 128, faces: [{ image: "I", eye: true }, { image: "E" }] },
 };
